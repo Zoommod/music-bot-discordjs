572dfa189ad6d3ce0bea802b28c588e97b6b2ae6
Zoommod <vitor.sacramento09@gmail.com>
2024-01-19 19:00:26 -0300

    ADD: implementando a função completa da fila de músicas

--- commands/queue.js
+++ commands/queue.js
@@ -6,6 +6,28 @@ module.exports = {
         .setName("fila")
         .setDescription("Mostra as 10 primeiras musicas da fila"),
     execute: async ({client, interaction}) => {
-        
+        const queue = client.player.getQueue(interaction.guild);
+
+        if(!queue || !queue.playing){
+            await interaction.reply("Nao ha musicas tocando");
+            return;
+        }
+
+        const queueString = queue.tracks.slice(0, 10).map((song, i) => {
+            return `${i + 1}) [${song.duration}]\` ${song.title} - <@${song.requestedBy.id}>`;
+        }).join("\n");
+
+        const currentSong = queue.current
+
+        await interaction.reply({
+            embeds: [
+                new MessageEmbed()
+                    .setDescription(`**Currently Playing**\n` + 
+                        (currentSong ? `\`[${currentSong.duration}]\` ${currentSong.title} - <@${currentSong.requestedBy.id}>` : "None") +
+                        `\n\n**Queue**\n${queueString}`
+                    )
+                    .setThumbnail(currentSong.setThumbnail)
+            ]
+        })
     }
 }   
